5f40b92d70d33a4ce239cbb88320bdc0497a1a09
Sergey23131 <semno321@gmail.com>
2021-07-16 19:29:27 +0300

    something wrong

---
+++ JS_Practice/local/index.js
@@ -0,0 +1,137 @@
+/*let str = 'Hello';
+let num = 123;
+let flag = true;
+let txt = 'true';
+
+console.log(typeof str);
+console.log(typeof num);
+console.log(typeof flag);
+console.log(typeof txt);
+
+let a_1 = 12;
+let a_2 = 2;
+let a_3 = 90;
+let a_4 = 3;
+let a_5 = 11;
+
+console.log((a_1 * a_4) - a_2);
+console.log((a_1 * a_4) / a_4);
+console.log(a_3 - (a_1 * a_4) + a_1);
+console.log((a_3 * a_4) / a_4);
+console.log(a_3 - a_4);
+
+let a_6 = 5 % 3;
+let a_7 = 3 % 5;
+let a_8 = 5 + '3';
+let a_9 = '5' - 3;
+let a_10 = 75 + 'кг';
+
+let height = 23;
+let width = 10;
+
+let s;
+s = height * width;
+
+let height_c = 10;
+let d_c = 4;
+
+let v;
+v = height_c * d_c;
+
+let n = 3;
+let m = 4;
+
+let k;
+k = (Math.sqrt(Math.pow(n, 2) + Math.pow(m, 2)));
+
+
+let name = 'Serhii Stetsenko';
+let age = 21;
+let hobby = 'music';
+alert("Serhii Stetsenko \n 21\n music");
+
+let str_1 = 'Who ';
+let str_2 = "are ";
+let str_3 = 'you?';
+
+let concatenation;
+
+concatenation = str_1 + str_2 + str_3;
+
+document.write(`${concatenation} `);
+
+let str_5 = "20";
+let a_11 = 5;
+document.write(str_5 + a_11 + "<br/>");
+document.write(str_5 - a_11 + "<br/>");
+document.write(str_5 * "2" + "<br/>");
+document.write(str_5 / 2 + "<br/>");
+
+let number_1=prompt();
+let number_2=prompt();
+
+let result= +number_1+ +number_2;
+alert(result);
+
+let name_1 = prompt('Your name?');
+let surname = prompt('Your surname?');
+let age_1 = prompt('Your ages?');
+
+alert("Dear "+name_1+surname+", take my congratulations, you are "+age_1);*/
+////////////////////////////////////////////////////////
+/*et first= prompt('put first number');
+let second= prompt('put second number');
+let thirdth= prompt('put thirdth number');
+
+let arr = [];
+
+if (first>second&&first>thirdth){
+arr[0]=first;
+}else if(second>first&&second>thirdth){
+  arr[0]=second;
+}
+else if(thirdth>first&&thirdth>second){
+  arr[0]=thirdth;
+}
+
+if (first>second&&first>thirdth){
+  arr[1]=first;
+}else if(second>first&&second>thirdth){
+  arr[1]=second;
+}
+else if(thirdth>first&&thirdth>second){
+  arr[1]=thirdth;
+}
+
+if (first>second&&first>thirdth){
+  arr[2]=first;
+}else if(second>first&&second>thirdth){
+  arr[2]=second;
+}
+else if(thirdth>first&&thirdth>second){
+  arr[2]=thirdth;
+}
+
+alert(arr);*/
+////////////////////////////////////////////////////////
+/*
+let color = prompt('Введите цвет светофора');
+let isRoadClear=prompt('Машины есть?');
+if (color == 'зеленый'&& isRoadClear=="нет") {
+  alert("ИДИ");
+}
+else if (color == 'зеленый'&& isRoadClear=="да"){
+    alert("Подожди пока нарушители проедут");
+}else if (color == 'желтый'&& isRoadClear=="да"){
+  alert("Подожди");
+}
+else if (color == 'желтый'&& isRoadClear=="нет") {
+  alert("Всё равно жди");
+}
+else if (color == 'красный' && isRoadClear=="нет"){
+    alert("Стой все равно");
+}else if (color == 'красный' && isRoadClear=="да"){
+  alert("Стой и жди");
+}else {
+  alert("Делай что хочешь");
+}*/
